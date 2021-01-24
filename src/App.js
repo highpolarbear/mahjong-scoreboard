26e6acda645ac81be3e367ef0757494dfc5f437b
Sin, Ka Wang <ka.sin@kcl.ac.uk>
2021-01-24 21:53:12 +0000

    Fix router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Header from "./pages/header/header";
 import Routes from "./route";
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Routes />
     </div>
   );
