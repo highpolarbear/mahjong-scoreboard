c2eb1507e3247c498d41cad92a71444713219ade
Sin, Ka Wang <ka.sin@kcl.ac.uk>
2021-01-25 00:55:25 +0000

    connect to db

--- server/router.js
+++ server/router.js
@@ -7,6 +7,7 @@ var jsonParser = bodyParser.json();
 
 // Routes
 router.post("/create-user", jsonParser, queries.createUser); // {"name": "username"}
+router.get("/get-all-user", jsonParser, queries.getAllUser); // {}
 router.get("/find-user", jsonParser, queries.findUser); // {"name": "username"}
 router.post("/log-match", jsonParser, queries.logMatch); // {"score":"3", "isSelfDraw": "false", "players": {"winner":"id", "loser":["id"]}}
 router.get("/find-match-by-id", jsonParser, queries.findMatchById); // {"id": "id"}
--- src/route.js
+++ src/route.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import { wrapHistory } from "oaf-react-router";
+import styled from "styled-components";
 import Header from "./pages/header/header";
 import Home from "./pages/home/home";
 import Dashboard from "./pages/dashboard/dashboard";
@@ -9,18 +10,25 @@ import Dashboard from "./pages/dashboard/dashboard";
 const history = createBrowserHistory();
 wrapHistory(history);
 
+const StyledWrapper = styled.div`
+  max-width: 980px;
+  margin: 0 auto;
+`;
+
 const Routes = () => {
   return (
     <Router history={history}>
       <Header />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/dashboard" exact component={Dashboard} />
-        <Route path="/create-user" exact component={Home} />
-        <Route path="/create-match" exact component={Home} />
-        <Route path="/view-match" exact component={Home} />
-        <Route component={Dashboard} />
-      </Switch>
+      <StyledWrapper>
+        <Switch>
+          <Route path="/" exact component={Dashboard} />
+          <Route path="/dashboard" exact component={Dashboard} />
+          <Route path="/create-user" exact component={Home} />
+          <Route path="/create-match" exact component={Home} />
+          <Route path="/view-match" exact component={Home} />
+          <Route component={Dashboard} />
+        </Switch>
+      </StyledWrapper>
     </Router>
   );
 };
